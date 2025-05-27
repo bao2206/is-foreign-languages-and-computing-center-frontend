3646d476e5954ec371d2fffd0560e7792f99231c
wessley9 <114457599+wessley9@users.noreply.github.com>
2025-05-27 18:54:29 +0700

    fix layout course and class

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
 // src/components/ui/button.js
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-export const Button = ({ children, className = '', ...props }) => (
-  <button className={`px-4 py-2 rounded bg-blue-600 text-gray ${className}`} {...props}>
+export const Button = ({ children, className = "", ...props }) => (
+  <button
+    className={`px-4 py-2 rounded bg-blue-600 text-white ${className}`}
+    {...props}
+  >
     {children}
   </button>
 );
